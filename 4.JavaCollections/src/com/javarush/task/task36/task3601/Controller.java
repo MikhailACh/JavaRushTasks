78f768ceb8230a386cb254e260123492a3bec569
MikhailACh <chupshevma@mail.ru>
2018-06-04 19:08:57 +0800

    Task 3609 added

--- 4.JavaCollections/src/com/javarush/task/task36/task3601/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task36/task3601/Controller.java
@@ -1,4 +1,12 @@
 package com.javarush.task.task36.task3601;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
+    Model model = new Model();
+
+    public List<String> onDataListShow() {
+        return model.getStringDataList();
+    }
 }
