1e1c93081cebdbd538924413dbc84a7d682fb58c
MikhailACh <chupshevma@mail.ru>
2018-06-17 19:04:03 +0800

    Task01 added

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println("Однажды в студеную зимнюю пору");
+        System.out.println("Я из лесу вышел, был сильный мороз.");
+        System.out.println("Гляжу: поднимается медленно в гору");
+        System.out.println("Лошадка, везущая хворосту воз.");
+        //напишите тут ваш код
+    }
+}
--- 1.JavaSyntax/src/com/javarush/task/task01/task0140/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0140/Solution.java
@@ -1,14 +1,13 @@
 package com.javarush.task.task01.task0140;
 
-import java.util.Scanner;
-
 /* 
 Выводим квадрат числа
 */
 
 public class Solution {
+    public static int number = 25;
+
     public static void main(String[] args) {
-        int a;
-        //напишите тут ваш код
+        System.out.println(number * number);
     }
 }
