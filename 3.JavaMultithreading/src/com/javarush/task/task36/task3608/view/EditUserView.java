98c50ccea460eed5372c0b9ad1694b84241c2d6c
MikhailACh <chupshevma@mail.ru>
2019-07-18 11:41:48 +0300

    MVC: DAO, DB, FakeModel

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/EditUserView.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task36.task3608.view;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.ModelData;
+
+import java.util.List;
+
+public class EditUserView implements View {
+    private Controller controller;
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("User to be edited:");
+
+        User user = modelData.getActiveUser();
+        System.out.println("\t" + String.format("User{name='%s', id=%d, level=%d}", user.getName(), user.getId(), user.getLevel()));
+
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void fireEventShowAllUsers() {
+        controller.onShowAllUsers();
+    }
+
+    public void fireEventShowDeletedUsers() {
+        controller.onShowAllDeletedUsers();
+    }
+
+    public void fireEventUserDeleted(long id) {
+        controller.onUserDelete(id);
+    }
+
+    public void fireEventUserChanged(String name, long id, int level) {
+        controller.onUserChange(name, id, level);
+    }
+}
