fa751820b593829b6aa4a63c6cee9a0ceeaf5d05
MikhailACh <chupshevma@mail.ru>
2020-02-21 15:54:54 +0400

    Tasks 2701, 2702 has done

---
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2701/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task27.task2701;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Избавляемся от меток
+*/
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String string = reader.readLine();
+        String substring = reader.readLine();
+
+        if (isSubstringPresent(substring, string)) {
+            System.out.println("String: \n" + substring + "\nis present in the string: \n" + string);
+        } else {
+            System.out.println("String: \n" + substring + "\nis not present in the string: \n" + string);
+        }
+    }
+
+    static boolean isSubstringPresent(String substring, String string) {
+        int diff = string.length() - substring.length();
+
+        for (int i = 0; i <= diff; i++)
+            for (int k = i; string.charAt(k) == substring.charAt(k - i); k++)
+                if (k == i + (substring.length() - 1))
+                    return true;
+
+        return false;
+    }
+}
